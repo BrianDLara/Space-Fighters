80dd4fe22a6acdfcfb76a51b300bb3053d218b62
Brian Lara <Briandlara1@gmail.com>
2022-10-05 16:46:03 -0400

    added movements to the aliens

--- app.js
+++ app.js
@@ -13,36 +13,101 @@ let squares = Array.from(cells);
 squares[spaceshipIndex].classList.add('spaceship')
 let spaceship = squares[spaceshipIndex]
 let shoot;
-
-
+let trashCan = []
 
 
 //green aliens
-for(g = 0 ; g < cells.length - 270; g++){
-    cells[g].classList.add('green-alien', 'alien')
+const addGreenAliens = () => { 
+  for(let g = 0 ; g < aliens.length -45; g++){
+    if(!trashCan.includes(g)){   
+    squares[aliens[g]].classList.add('green-alien')
+    }
+  }
 }
-//red aliens
-for(r = 19 ; r < cells.length - 251; r++){
-    cells[r].classList.add('red-alien', 'alien')
-}
-//yellow aliens
-for(y = 38 ; y < cells.length - 232; y++){
-    cells[y].classList.add('yellow-alien', 'alien')
-}
-//purple aliens
-
-for(p = 57 ; p < cells.length - 213; p++){
- cells[p].classList.add('purple-alien', 'alien')
+const removeGreenAliens = () => { 
+  for(let g = 0 ; g < aliens.length -45; g++){
+      squares[aliens[g]].classList.remove('green-alien')
+  }
 }
 
-let purpleAlien = document.getElementsByClassName('purple-alien')
-console.log(purpleAlien)
+const addRedAliens = () => { 
+  for(let r = 15 ; r < aliens.length -30; r++){
+      if(!trashCan.includes(r)){ 
+        squares[aliens[r]].classList.add('red-alien')
+      }
+  }
+}
+const removeRedAliens = () => { 
+  for(let r = 15 ; r < aliens.length -30; r++){
+      squares[aliens[r]].classList.remove('red-alien')
+  }
+}
+
+const addYellowAliens = () => { 
+  for(let y = 30 ; y < aliens.length -15; y++){
+    if(!trashCan.includes(y)){ 
+      squares[aliens[y]].classList.add('yellow-alien')
+    }
+  }
+}
+const removeYellowAliens = () => { 
+  for(let y = 30 ; y < aliens.length -15; y++){
+      squares[aliens[y]].classList.remove('yellow-alien')
+  }
+}
+
+const addPurpleAliens = () => { 
+  for(let p = 45 ; p < aliens.length; p++){
+    if(!trashCan.includes(p)){  
+    squares[aliens[p]].classList.add('purple-alien')
+    }
+  }
+}
+const removePurpleAliens = () => { 
+  for(let p = 45 ; p < aliens.length; p++){
+      squares[aliens[p]].classList.remove('purple-alien')
+  }
+}
+
+let aliens = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14,
+  19, 20 , 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ,31, 32, 33,
+  38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+  57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71
+  
+]
+
+const alien = document.querySelectorAll('.alien')
+addGreenAliens()
+addRedAliens()
+addYellowAliens()
+addPurpleAliens()
+const moveAliens = () => {
+  const leftEdge = aliens[0] % containerWidth === 0;
+  const rightEdge = aliens[alien.length - 1] % containerWidth === containerWidth - 1;
+  removeGreenAliens()
+  removeRedAliens()
+  removeYellowAliens()
+  removePurpleAliens()
+  for(let i = 0; i < aliens.length; i++){
+   aliens[i] += 1
+  }
+  addGreenAliens()
+  addRedAliens()
+  addYellowAliens()
+  addPurpleAliens()
+  
+  console.log(aliens)
+
+}
+setInterval(moveAliens, 1000)
 
 
 const moveSpaceship = (e) => {
     let missileIndex = spaceshipIndex
     // removes the last spaceship class, if not they just clone themselves
     squares[spaceshipIndex].classList.remove('spaceship')
+    squares[missileIndex].classList.remove('missile')
     const moveMissile = () => {
         if (squares[missileIndex] !== undefined){ 
         squares[missileIndex].classList.remove('missile')
@@ -53,21 +118,23 @@ const moveSpaceship = (e) => {
         if (squares[missileIndex].classList.contains('purple-alien')){
                 squares[missileIndex].classList.remove('purple-alien') 
                 squares[missileIndex].classList.remove('missile') 
+               
                 clearInterval(shoot)
         }else if(squares[missileIndex].classList.contains('yellow-alien')){
             squares[missileIndex].classList.remove('yellow-alien')
             squares[missileIndex].classList.remove('missile') 
-            clearInterval(shoot)
+            
         }else if(squares[missileIndex].classList.contains('red-alien')){
             squares[missileIndex].classList.remove('red-alien') 
             squares[missileIndex].classList.remove('missile') 
-            clearInterval(shoot)
+           
         }else if(squares[missileIndex].classList.contains('green-alien')){
             squares[missileIndex].classList.remove('green-alien') 
             squares[missileIndex].classList.remove('missile') 
-            clearInterval(shoot)
-
+            
         }
+        const removeAliens = aliens.indexOf(missileIndex)
+        trashCan.push(removeAliens)
         
         }
         
@@ -96,15 +163,12 @@ const moveSpaceship = (e) => {
     //after breaking out of the switch statement create a
     // spaceship class on the new div index
     squares[spaceshipIndex].classList.add('spaceship')
-    squares[missileIndex].classList.remove('missile')
+    
 }
 
 
-// console.log(hit)
+
 
 
 
 window.addEventListener('keydown', moveSpaceship)
-
-
-
