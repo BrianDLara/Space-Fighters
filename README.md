6feb36f55cf0bda26b9359daaceff399491cdd2d
Brian Lara <Briandlara1@gmail.com>
2022-10-06 16:27:13 -0400

    restored file from error lost 4 commits

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ https://www.clipartmax.com/download/m2i8m2H7Z5i8i8H7_space-invaders-extreme-2-po
 
 https://unsplash.com/photos/Ef6iL87-vOA
 
-Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=91523">Pixabay</a>
+<!-- Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=91523">Pixabay</a> -->
+
+Video by Oleg Gamulinskii from Pexels: https://www.pexels.com/video/shades-of-lights-in-the-outer-space-2611250/
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const backgroundMusic = new Audio('../sounds/space.mp3')
 backgroundMusic.loop = true
-
 const shootSound = new Audio('../sounds/shoot2.mp3')
-
 const gameOver = new Audio('../sounds/dead.mp3')
+
 const container = document.querySelector('#container')
 const playerScore = document.querySelector('#player-score')
 const hiScore = document.querySelector('#hi-score')
+const message = document.querySelector('#message')
+const restartBtn = document.querySelector('#restart-btn')
 
+//Creates grid squares with a for loop 19 X 19
 for (let i=0; i < 285 ; i++){
     let cell = document.createElement('div')
     cell.id = `div` + i;
@@ -15,21 +17,24 @@ for (let i=0; i < 285 ; i++){
     container.appendChild(cell)
 }
 
+const cells = document.querySelectorAll('.cells')
 let spaceshipIndex = 256
 let containerWidth = 19;
-const cells = document.querySelectorAll('.cells')
+let highestScore = 0;
+let score = 0
+let num = 1;
+let movingRight = true
 let squares = Array.from(cells);
 squares[spaceshipIndex].classList.add('spaceship')
 let spaceship = squares[spaceshipIndex]
-let shoot;
-let trashCan = []
+let graveyard = []
 
 backgroundMusic.play();
 //green aliens
 const addGreenAliens = () => { 
   for(let g = 0 ; g < aliens.length -45; g++){
-    if(!trashCan.includes(g)){   
-    squares[aliens[g]].classList.add('green-alien')
+    if(!graveyard.includes(g)){   
+    squares[aliens[g]].classList.add('green-alien', 'alien')
     }
   }
 }
@@ -39,9 +44,10 @@ const removeGreenAliens = () => {
   }
 }
 
+//red aliens
 const addRedAliens = () => { 
   for(let r = 15 ; r < aliens.length -30; r++){
-      if(!trashCan.includes(r)){ 
+      if(!graveyard.includes(r)){ 
         squares[aliens[r]].classList.add('red-alien', 'alien')
       }
   }
@@ -52,9 +58,10 @@ const removeRedAliens = () => {
   }
 }
 
+//yellow aliens
 const addYellowAliens = () => { 
   for(let y = 30 ; y < aliens.length -15; y++){
-    if(!trashCan.includes(y)){ 
+    if(!graveyard.includes(y)){ 
       squares[aliens[y]].classList.add('yellow-alien', 'alien')
     }
   }
@@ -65,10 +72,10 @@ const removeYellowAliens = () => {
   }
 }
 
-
+//purple aliens
 const addPurpleAliens = () => { 
   for(let p = 45 ; p < aliens.length; p++){
-    if(!trashCan.includes(p)){  
+    if(!graveyard.includes(p)){  
     squares[aliens[p]].classList.add('purple-alien', 'alien')
     }
   }
@@ -86,38 +93,61 @@ let aliens = [
   57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71
   
 ]
-
-
-const alien = document.querySelectorAll('.alien')
+const startGame = () => { 
 addGreenAliens()
 addRedAliens()
 addYellowAliens()
 addPurpleAliens()
 //Adds movement to any type of alien
 const moveAliens = () => {
-  const leftEdge = aliens[0] % containerWidth === 0;
-  const rightEdge = aliens[alien.length - 1] % containerWidth === containerWidth - 1;
+  const leftBorder = aliens[0] % containerWidth === 0;
+  const rightBorder = aliens[aliens.length - 1] % containerWidth === containerWidth - 1;
+
   removeGreenAliens()
   removeRedAliens()
   removeYellowAliens()
   removePurpleAliens()
+
+
+
+  if(rightBorder && movingRight) {
+    for(let i = 0; i < aliens.length; i++){
+      // + 1 forced the aliens to start at the border
+      aliens[i] += containerWidth + 1
+      // change the direction to the left
+      num = -1
+      //break didn't work so I added a boolean and that seemed to help
+      movingRight = false
+    }
+  }
+
+  if(leftBorder && !movingRight) {
+    for(let i = 0; i < aliens.length; i++){
+      // - 1 forces the aliens to the left border
+      aliens[i] += containerWidth - 1
+      //changes the direction to the right
+      num = 1
+      movingRight = true
+    }
+  }
+
   for(let i = 0; i < aliens.length; i++){
-    aliens[i] += 1
+    aliens[i] += num
   }
   addGreenAliens()
   addRedAliens()
   addYellowAliens()
   addPurpleAliens()
   
- 
-  //every interval the game checks for a game over, a win, accumulates point, and checks for a hi-score
-  checkForGameOver();
-  checkForWin();
-  addScore();
   
-
+  //every interval the game checks for a game over,and a win
+  // checkForWin();
+  checkForGameOver();
+  
 }
-alienSpeed = setInterval(moveAliens, 6000)
+alienSpeed = setInterval(moveAliens, 300)
+}
+
 
 
 const moveSpaceship = (e) => {
@@ -146,6 +176,7 @@ const moveSpaceship = (e) => {
 }
 
 const shootMissiles = (e) => {
+  let shoot;
   let missileIndex = spaceshipIndex
   squares[missileIndex].classList.remove('missile')
 
@@ -156,68 +187,103 @@ const shootMissiles = (e) => {
     squares[missileIndex].classList.add('missile')
     
     
+    
       if (squares[missileIndex].classList.contains('purple-alien')){
-              squares[missileIndex].classList.remove('purple-alien') 
-              squares[missileIndex].classList.remove('missile') 
-            
-              clearInterval(shoot)
+        squares[missileIndex].classList.remove('purple-alien') 
+        squares[missileIndex].classList.remove('missile') 
+        clearInterval(shoot)
+        addScore();
+        
       }else if(squares[missileIndex].classList.contains('yellow-alien')){
           squares[missileIndex].classList.remove('yellow-alien')
           squares[missileIndex].classList.remove('missile') 
-          
+          clearInterval(shoot)
+          addScore()
+
       }else if(squares[missileIndex].classList.contains('red-alien')){
           squares[missileIndex].classList.remove('red-alien') 
           squares[missileIndex].classList.remove('missile') 
-        
+          clearInterval(shoot)
+          addScore()
+
       }else if(squares[missileIndex].classList.contains('green-alien')){
           squares[missileIndex].classList.remove('green-alien') 
           squares[missileIndex].classList.remove('missile') 
-          
+          clearInterval(shoot)
+          addScore()
       }
+      if (squares[missileIndex].classList.contains('alien')){ 
         const removeAliens = aliens.indexOf(missileIndex)
-        trashCan.push(removeAliens)
+        graveyard.push(removeAliens)
+      }
     
     }
   }
   switch (e.key) {
     case "ArrowUp":
       shootSound.play()
-      shoot = setInterval(moveMissile, 150)
-      break;
+      shoot = setInterval(moveMissile, 250)
+      
   }
+  
 }
 
 const checkForGameOver = () => {
   if(squares[spaceshipIndex].classList.contains('alien', 'spaceship')){
     backgroundMusic.pause()
     gameOver.play()
-    console.log(`Game Over`)
-    clearInterval(shoot)
+    message.innerText = `GAME OVER`
     clearInterval(alienSpeed)
-    
-  }
+    hiScoreCount()
+    removeGreenAliens()
+    removeRedAliens()
+    removeYellowAliens()
+    removePurpleAliens()}
+  //  }else if(){
+
+  //  }
 }
 
-const checkForWin = () => {
-  if(!squares.classList.contain('alien')){
-    console.log(`You Win`)
-  }
+const reset = () => {
+  aliens = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14,
+    19, 20 , 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ,31, 32, 33,
+    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+    57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71
+    
+  ]
+  startGame();
+  
 }
+
+// if all the aliens are on the graveyard, you win
+// const checkForWin = () => {
+//   if(!squares.classList.contains('aliens')){
+//     console.log(`You Win`)
+//     hiScoreCount()
+
+//   }
+// }
+
 
 const addScore = () => {
-  let sumScore = 0
-  if(squares[missileIndex].classList.contains('alien', 'missile')){
-    sumScore += 1
-    playerScore.innerText = sumScore;
-  }
+    score += 10
+    playerScore.innerText = score;
 }
 
+const hiScoreCount = () => {
+  if(score > highestScore ){
+      highestScore = score
+      hiScore.innerText = score
+  }
+  return highestScore
+}
 
-
-console.log(squares)
-
-
+//moves the all aliens 1 square at a time every 300ms
 
 
 window.addEventListener('keydown', moveSpaceship)
 window.addEventListener('keydown', shootMissiles)
+restartBtn.addEventListener('click', reset)
+
+startGame();
